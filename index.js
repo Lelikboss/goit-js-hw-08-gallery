5ba7eb67c036335125edbe117d8f20707d3dc8d5
Alexey Alexeew <lelikboss97@gmail.com>
2021-07-22 17:36:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 import galleryItems from "./app.js";
 
 const galleryContainer = document.querySelector(".js-gallery");
-const imageMarkup = createGalleryItems(galleryItems);
 const lightboxEl = document.querySelector(".lightbox");
 const closeBtnEl = document.querySelector('[data-action="close-lightbox"]');
 const imageModalEl = document.querySelector(".lightbox__image");
-const imgItemEl = document.querySelector(".gallery__item");
 const backdropEl = document.querySelector(".lightbox__overlay");
-const galleryImgEl = document.querySelector(".gallery__image");
+const imageMarkup = createGalleryItems(galleryItems);
 galleryContainer.insertAdjacentHTML("beforeend", imageMarkup);
 galleryContainer.addEventListener("click", onGallaryContainerClick);
 closeBtnEl.addEventListener("click", onCloseBtn);
 backdropEl.addEventListener("click", onBackdropClick);
-
 function createGalleryItems(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }, index) => {
@@ -36,100 +33,69 @@ function createGalleryItems(galleryItems) {
     })
     .join("");
 }
-function onGallaryContainerClick(evt) {
-  if (!evt.target.classList.contains("gallery__image")) {
+function onGallaryContainerClick(e) {
+  if (!e.target.classList.contains("gallery__image")) {
     return;
   }
   window.addEventListener("keydown", onEscapePress);
-  onClickImage(evt);
+  onClickImage(e);
 }
-
-function onClickImage(evt) {
-  evt.preventDefault();
-  imageModalEl.dataset.index = evt.target.dataset.index;
+function onClickImage(e) {
+  e.preventDefault();
+  imageModalEl.dataset.index = e.target.dataset.index;
   addClassIsOpen();
-  addImgAttribute(evt);
+  window.addEventListener("keydown", toTheSide);
+  addImgAttribute(e);
 }
-function addImgAttribute(evt) {
-  const attrImg = evt.target.dataset.source;
-  console.log(evt.target.classList.contains("gallery__image"));
+function addImgAttribute(e) {
+  const attrImg = e.target.dataset.source;
   imageModalEl.src = attrImg;
-  console.log(imageModalEl.src);
 }
 function addClassIsOpen() {
   lightboxEl.classList.add("is-open");
 }
-function onCloseBtn(e) {
+function onCloseBtn() {
   lightboxEl.classList.remove("is-open");
   emptyImgAtribute();
 }
 function emptyImgAtribute() {
   if (!lightboxEl.classList.contains("is-open")) {
     imageModalEl.setAttribute("src", "");
-    console.log(imageModalEl);
   }
 }
 function onBackdropClick(e) {
-  console.log("ok");
   onCloseBtn(e);
 }
-
 function onEscapePress(e) {
-  console.log(e);
   if (e.code === "Escape") {
     onCloseBtn(e);
   }
 }
-// function rightTurn() {
-//   if (activImg.length < galleryItems.length) {
-//     console.log(activImg.length);
-//   }
-// }
-// imageModalEl.setAttribute("src", "");
-// console.log(imageModalEl);
-// window.addEventListener("keydown", onRightArrowPress);
-// function onRightArrowPress(e) {
-//   if (e.code === "ArrowRight") {
-//     for (let i = 0; i < galleryItems.length; i += 1) {
-//       imageModalEl.setAttribute("src", "");
-//       console.log(imageModalEl);
-//     }
-//     console.log(e);
-//     console.log(imageModalEl);
-//   }
-//   // for (let i = 0; i < galleryItems.length; i += 1) {
-//   // }
-// }
-
-/////////////////
-window.addEventListener("keydown", (e) => {
+function toTheSide(e) {
   if (e.code === "ArrowLeft") {
     onArrowLeft();
   }
   if (e.code === "ArrowRight") {
     onArrowRight();
   }
-});
-
+}
 function onArrowLeft() {
   let index = Number(imageModalEl.dataset.index);
   if (index === 0) {
-    newSrc(galleryItems.length - 1);
+    findNewIndex(galleryItems.length - 1);
     return;
   }
-  newSrc(index, -1);
+  findNewIndex(index, -1);
 }
 function onArrowRight() {
-  let index = +imageModalEl.dataset.index;
+  let index = Number(imageModalEl.dataset.index);
   if (index === galleryItems.length - 1) {
-    newSrc(0);
+    findNewIndex(0);
     return;
   }
-  newSrc(index, 1);
+  findNewIndex(index, 1);
 }
-
-function newSrc(index, step = 0) {
-  imageModalEl.dataset.index = `${index + step}`;
-  console.log(imageModalEl);
-  imageModalEl.src = galleryItems[index + step].original;
+function findNewIndex(index, i = 0) {
+  imageModalEl.dataset.index = `${index + i}`;
+  imageModalEl.src = galleryItems[index + i].original;
 }
